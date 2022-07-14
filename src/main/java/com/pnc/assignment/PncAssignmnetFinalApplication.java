c80761c69dd1d6c170a00b99d56d9cc7b32b7940
Asad Vhora <asad.vhora@gmail.com>
2022-07-14 15:23:10 -0300

    project created

---
+++ src/main/java/com/pnc/assignment/PncAssignmnetFinalApplication.java
@@ -0,0 +1,13 @@
+package com.pnc.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncAssignmnetFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PncAssignmnetFinalApplication.class, args);
+	}
+
+}
